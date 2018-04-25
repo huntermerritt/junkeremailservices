b887bce3041fcfbf87c1f358fd403fe95d4a6762
Hunter <hunteramerritt1997@gmail.com>
2018-04-25 18:18:59 -0500

    Got rid of the try and catch blocks and fixed the tabbing issues.

--- Junkerwork.py
+++ Junkerwork.py
@@ -15,27 +15,28 @@ def hello_world():
     matches = read_email_from_gmail(str(id))
     email = "junkeremailservices" + str(id) + "@gmail.com"
     print(matches)
-    print(email)
+
     return render_template('index.html', emailmatch=read_email_from_gmail(str(id)), email="junkeremailservices+" + id + "@gmail.com")
 
 
 def read_email_from_gmail(id):
-    try:
-	FROM_EMAIL = "junkeremailservices@gmail.com"
-	FROM_PWD = "junkeremail"
+    #try:
+        from_email = "junkeremailservices@gmail.com"
+        from_pwd = "junkeremail"
         mail = imaplib.IMAP4_SSL("smtp.gmail.com")
-        mail.login(FROM_EMAIL,FROM_PWD)
+        mail.login(from_email,from_pwd)
         mail.select('inbox')
 
         type, data = mail.search(None, 'ALL')
         mail_ids = data[0]
 
-        id_list = mail_ids.split()   
+        id_list = mail_ids.split()
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
-	
-	f = open("emailout.txt", "w+")
-	emails = []
+
+        f = open("emailout.txt", "w+")
+        emails = []
+
         for i in range(latest_email_id,first_email_id, -1):
             typ, data = mail.fetch(i, '(RFC822)' )
 
@@ -43,31 +44,29 @@ def read_email_from_gmail(id):
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
                     email_subject = msg['subject']
-		    print(msg.keys())
+                    print(msg.keys())
                     email_from = str(msg['from'])
-		    email_to = msg['to']
-		    email_body = ""
-		    if msg.is_multipart():
-		    	for payload in msg.get_payload():
-				email_body = email_body + payload + "\n"
-		    p = re.compile('\d+')
-		    search = p.search(email_to)
-		   
-		    if search:
-		    	email_id = search.group()
-		    	if email_id == id:
-                    		f.write('From : ' + email_from + '\n')
-                    		f.write('Subject : ' + email_subject + '\n')
-		    		f.write('T0 : ' + email_to + '\n')
-				emails.append("From : " + email_from + "\n" + "TO : " + email_to + "\n" + email_body)
-	endstr = ""
-	for temp in emails:
-		endstr = endstr + "<br>" + "<div class=message>" + temp + "</div>" + "<br>"			
-	return endstr
-		
+                    email_to = msg['to']
+                    email_body = ""
+                    if msg.is_multipart():
+                        for payload in msg.get_payload():
+                            email_body = email_body + payload + "\n"
+                    p = re.compile('\d+')
+                    search = p.search(email_to)
 
-    except Exception, e:
-        print str(e)
+                    if search:
+                        email_id = search.group()
+                        if email_id == id:
+                            f.write('From : ' + email_from + '\n')
+                            f.write('Subject : ' + email_subject + '\n')
+                            f.write('T0 : ' + email_to + '\n')
+                            emails.append("From : " + email_from + "\n" + "TO : " + email_to + "\n" + email_body)
+        endstr = ""
+        for temp in emails:
+            endstr = endstr + "<br>" + "<div class=message>" + temp + "</div>" + "<br>"
+        return (endstr)
+    #except Exception:
+    #    print(str(Exception) + " WRONG")
 
 
 if __name__ == '__main__':
