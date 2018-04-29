70619594ea9d78d86875b2692d75d9490bd44cfe
Hunter <hunteramerritt1997@gmail.com>
2018-04-29 01:34:03 -0500

    Rewriting the Junker.py.

--- Junker.py
+++ Junker.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import poplib, email
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():
+
+    mailserver = 'pop.gmail.com'
+    emailid = 'junkeremailservices@gmail.com'
+    emailpass = 'junkeremail'
+
+    myconnection = poplib.POP3_SSL(mailserver)
+
+    myconnection.user(emailid)
+    myconnection.pass_(emailpass)
+
+    emailinfo = myconnection.stat()
+
+    numbermail = emailinfo[0]
+
+    for i in range(numbermail):
+        for emails in myconnection.retr(i + 1)[1]:
+            print emails
+
     return render_template('index.html', email='junker', emailmatch='emailmatch')
 
 
